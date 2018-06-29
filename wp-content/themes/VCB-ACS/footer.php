ee2ad65efb00d211dd61bf5cff6242a25ddb7fc0
nelep90 <nenad.p@codeur.online>
2018-06-29 11:51:53 +0200

    footer ok

--- wp-content/themes/VCB-ACS/footer.php
+++ wp-content/themes/VCB-ACS/footer.php
@@ -1,10 +1,10 @@
-<footer>
-      <p>Copyright &copy; 2017</p>
-      <div class="rigth_footer">
-       <img class="footer_lg" src="images/logo_ACS_slide.png">
-       <img class="footer_lg" src="images/logo_defaut.png">
-    </div>
-    </footer>
+<footer class="footer"> 
+<p class="left_footer">Copyright &copy; 2017</p>  
+  <section class="right_footer">   
+      <img class="footer_lg" src="/images/logo_ACS_slide.png">
+      <img class="footer_lg" src="/images/logo_defaut.png">    
+   </section>
+      </footer>
     <?php wp_footer(); ?>
   </body>
 </html>
