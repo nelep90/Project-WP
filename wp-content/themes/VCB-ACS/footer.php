645d6d952d9c3ceeac70d469a952630fc33755ca
nelep90 <nenad.p@codeur.online>
2018-06-29 10:20:08 +0200

    version avec header menu

---
+++ wp-content/themes/VCB-ACS/footer.php
@@ -0,0 +1,10 @@
+<footer>
+      <p>Copyright &copy; 2017</p>
+      <div class="rigth_footer">
+       <img class="footer_lg" src="images/logo_ACS_slide.png">
+       <img class="footer_lg" src="images/logo_defaut.png">
+    </div>
+    </footer>
+    <?php wp_footer(); ?>
+  </body>
+</html>
